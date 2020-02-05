34855ed1d400d5dea86fc5803bbc91923eab0f2e
amal almomani <amalalmomani19@gmail.com>
2020-02-05 14:00:12 +0200

    add prompt

--- javascript/addcontent.js
+++ javascript/addcontent.js
@@ -1,15 +1,17 @@
-
-var today = new Date();
-var hourNow = today.getHours();
+function ourgreeting (){
+    //input
+var hourNow = prompt("what is the time now?");
 var greeting;
-
-if (hourNow > 18) {
+//process
+if (hourNow > 18 && hourNow <= 24 ) {
     greeting = 'Good evening!';   
-} else if (hourNow > 12) {
+} else if (hourNow > 12 && hourNow <= 18 ) {
     greeting = 'Good afternoon!';
-}else if (hourNow > 0) {
+}else if (hourNow > 0 && hourNow <= 12 ) {
     greeting = 'Good morning!';
 }else {
     greeting = 'Welcome!';
+} return greeting;
 }
-document.write('<h3>' + greeting + '</h3>');
+var hello = ourgreeting();
+document.write('<h3>' + hello + '</h3>');
